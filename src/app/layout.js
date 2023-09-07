ff47719b925851d1a59d7f190fb89a2a5ee74983
Frank <yuxuan.shi@students.britannicaschool.hu>
2023-09-07 15:28:36 +0200

    some stuff

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+import Navbar from '@/components/navbar/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +15,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
 
       <body className={inter.className}>
-        hello
         {children}
       </body>
     </html>

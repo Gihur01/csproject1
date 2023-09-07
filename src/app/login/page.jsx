ef3d7f6667e4da0c8a82c2beb8ff35143590c1fb
Frank <yuxuan.shi@students.britannicaschool.hu>
2023-09-07 15:28:31 +0200

    stuff

---
+++ src/app/login/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <div>
+        <h2>ID:</h2>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Login
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,40 @@
+import Link from 'next/link'
 import React from 'react'
+
+const paths = [
+    {
+        id: 1,
+        title: 'Home',
+        path: '/'
+    },
+    {
+        id: 2,
+        title: 'About',
+        path: '/about'
+    },
+    {
+        id: 3,
+        title: 'Login',
+        path: '/login'
+    },
+    {
+        id: 5,
+        title: 'Profile',
+        path: '/profile'
+    },
+]
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+        {
+            paths.map(link => (
+                <Link href={link.path} key={link.id}>
+                    {link.title}
+                </Link>
+            ))
+        }
+    </div>
   )
 }
 

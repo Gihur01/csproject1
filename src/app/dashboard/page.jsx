9fe6784c22e018f7f2c0837eae45e6fe6841e61f
Frank <yuxuan.shi@students.britannicaschool.hu>
2023-09-18 14:28:32 +0200

    navbar and login page

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from "@/components/navbar/Navbar"
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+      <Navbar/>
+      Dashboard
+    </div>
   )
 }
 

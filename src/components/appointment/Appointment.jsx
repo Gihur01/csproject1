e862655bf603a902cd6d01a7cf03d38a80b442df
Frank <yuxuan.shi@students.britannicaschool.hu>
2023-10-17 10:54:38 +0200

    adjusted indentation

--- src/components/appointment/Appointment.jsx
+++ src/components/appointment/Appointment.jsx
@@ -5,57 +5,45 @@ import { Container, Box, Button, List, ListItem, ListItemButton, ListItemText, L
 
 const Appointment = ({ appointments }) => {
     if (appointments) {
-        return (
-            <div>
-                <Container sx={{
-                    marginTop: 2,
-                    display: 'inline-block',
-                    maxWidth: 800,
-                    maxHeight: 500,
-                    overflow: 'auto',
-                    border: 1,
-                }} >
-                    <List>
-                        {appointments.map(app => (
-                            <ListItem key={app.id} disablePadding>
-                                <ListItemButton>
-                                    {/* <ListItemIcon>
-                            </ListItemIcon> */}
-                                    <ListItemText primary={app.type} secondary={
-                                        <Box sx={{
-                                            display: 'flex',
-                                            alignItems: 'center',
-                                            justifyContent: 'space-between',
-                                            width: '50%',
-                                        }}>
-                                            <span>
-
-                                                {app.date}
-                                            </span>
-                                            <span>
-                                                {app.category}
-                                            </span>
-                                            <span>
-                                                {app.time}
-                                            </span>
-                                        </Box>
-                                    } />
-                                </ListItemButton>
-                            </ListItem>))}
-
-                    </List>
-
-                </Container>
-
-            </div>
-        )
+    return (
+        <div>
+            <Container sx={{
+                marginTop: 2,
+                display: 'inline-block',
+                maxWidth: 800,
+                maxHeight: 500,
+                overflow: 'auto',
+                border: 1,
+            }} >
+                <List>
+                    {appointments.map(app => (
+                        <ListItem key={app.id} disablePadding>
+                            <ListItemButton>
+                            <ListItemText primary={app.type} secondary={
+                                <Box sx={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'space-between',
+                                    width: '50%',
+                                }}>
+                                    <span>{app.date}</span>
+                                    <span>{app.category}</span>
+                                    <span>{app.time}</span>
+                                </Box>
+                                } />
+                            </ListItemButton>
+                        </ListItem>))}
+                </List>
+            </Container>
+        </div>
+    )
     }
     else {
-        return(
-            <Typography>
-                You have no appointments.
-            </Typography>
-        )
+    return(
+        <Typography>
+            You have no appointments.
+        </Typography>
+    )
     }
 
 }
